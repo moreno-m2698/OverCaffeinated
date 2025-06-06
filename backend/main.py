154133f8eb57797d4df9a5a88c1e5854f5c5e166
moreno-m2698 <moreno.m2698@gmail.com>
2025-06-05 20:24:43 -0400

    Added paths in api

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,32 @@
+import sqlite3
+
 from fastapi import FastAPI
 
-def main():
-    Over_Caffeinated = FastAPI()
+# Run using 'fastapi dev main.py'
 
-    
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def index():
+    return {"Hello": "World"}
+
+@app.get("/drink")
+def drinks():
+    # TODO: will allow user to see there drinks/add new drinks
+    return {"drinks": 0}
+
+@app.get("/login")
+def login():
+    # TODO: will log user in
+    return []
+
+@app.get("/logout")
+def logout():
+    # TODO: will log user out
+    return []
+
+@app.get("/caffeine")
+def caffeine():
+    # TODO: Will give you current amount of caffeine
+    return []
