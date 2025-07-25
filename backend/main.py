1583d68ca816cf2fc928aafd9efbc9543b9674d1
moreno.m2698 <moreno.m2698@gmail.com>
2025-07-25 09:12:29 -0400

    Adjusted backend mock data

--- backend/main.py
+++ backend/main.py
@@ -21,21 +21,24 @@ def read_index():
 # fastapi dev main.py
 
 class Drink:
-    def __init__(self, id, name, caffeine, date):
+    def __init__(self, id: int, name: str, caffeine: int, date: str): # Might need to create a datetime parser
         self.id = id
         self.name = name
         self.caffeine = caffeine
         self.date = date
 
+
+
 @app.get("/api/drinks")
 def get_drinks():
-    today = datetime.date.today()
-    dt_today = datetime.datetime.fromisoformat()
+
+    dt_yesterday: datetime.datetime = datetime.combine(date=datetime.date.today(), time=datetime.time(hour=0, minute=0, second=0, microsecond=0)) - datetime.timedelta(days=1)
 
     #ISO date time: YYYY-MM-DDTHH:mm:ss.SSSZ
-    # mockDrinkData = [
-    #     { id: 1, "name": "Espresso", "caffeine": 80, "date": yesterday.toISOString() },
-    #     { id: 2, "name": "Latte", caffeine: 100, date: yesterday.toISOString() },
-    #     { id: 3, name: "Cold Brew", caffeine: 200, date: yesterday.toISOString() },
-    # ]
-    return []
+    mockDrinkData = [
+        { 'id': 1, "name": "Espresso", "caffeine": 80, "date": dt_yesterday.isoformat() },
+        { 'id': 2, "name": "Latte", "caffeine": 100, "date": dt_yesterday.isoformat() },
+        { 'id': 3, "name": "Cold Brew", "caffeine": 200, "date": dt_yesterday.isoformat() },
+    ]
+    
+    return mockDrinkData
